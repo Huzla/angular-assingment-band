54260604ca5f45c9c02acd2afe4de73a32e3c57f
Lari Alakukku <lari@alakukku.net>
2023-11-11 22:11:43 +0200

    Added AddPlays tests

--- src/app/task1/services/band-enrichment.service.spec.ts
+++ src/app/task1/services/band-enrichment.service.spec.ts
@@ -20,11 +20,11 @@ describe('BandEnrichmentService', () => {
       const testData: IncompleteBand = {
         members: {
           current: [
-            {name: 'a', age: 1, plays: []},
-            {name: 'b', age: 2, plays: []},
+            { name: 'a', age: 1, plays: [] },
+            { name: 'b', age: 2, plays: [] },
           ],
           past: [
-              {name: 'c', age: 3, plays: []},
+              { name: 'c', age: 3, plays: [] },
           ]
         }
       };
@@ -39,11 +39,11 @@ describe('BandEnrichmentService', () => {
       const testData: IncompleteBand = {
         members: {
           current: [
-            {name: 'Aapo-Kalevi Jukanpoika', age: 1, plays: []},
-            {name: 'someGuy', age: 2, plays: []},
+            { name: 'Aapo-Kalevi Jukanpoika', age: 1, plays: [] },
+            { name: 'someGuy', age: 2, plays: [] },
           ],
           past: [
-              {name: 'WeIrD nAME', age: 3, plays: []},
+              { name: 'WeIrD nAME', age: 3, plays: [] },
           ]
         }
       };
@@ -57,13 +57,13 @@ describe('BandEnrichmentService', () => {
       const testData: IncompleteBand = {
         members: {
           current: [
-            {name: 'Zorro', age: 98, plays: []},
-            {name: 'someGuy', age: 1, plays: []},
-            {name: 'Aapo-Kalevi Jukanpoika', age: 12, plays: []},
+            { name: 'Zorro', age: 98, plays: [] },
+            { name: 'someGuy', age: 1, plays: [] },
+            { name: 'Aapo-Kalevi Jukanpoika', age: 12, plays: [] },
           ],
           past: [
-              {name: '', age: 12, plays: []},
-              {name: 'Thor', age: 12, plays: []},
+              { name: '', age: 12, plays: [] },
+              { name: 'Thor', age: 12, plays: [] },
           ]
         }
       };
@@ -76,19 +76,88 @@ describe('BandEnrichmentService', () => {
 
   describe('addPlays', () => {
     it('should work with empty current or past', () => {
+      const testData: IncompleteBand = {
+        members: {
+          current: [
+            { name: 'a', age: 1, plays: ['play1'] },
+            { name: 'b', age: 2, plays: ['play2', 'play1'] },
+          ],
+          past: [
+              { name: 'c', age: 3, plays: ['play2'] },
+              { name: 'd', age: 3, plays: [] }
+          ]
+        }
+      };
 
+      expect(Object.keys(service.addPlays(testData).plays).length).toEqual(2);
+      expect(Object.keys(service.addPlays({ members: { ...testData.members, current: [] } }).plays).length).toEqual(1);
+      expect(Object.keys(service.addPlays({ members: { ...testData.members, past: [] } }).plays).length).toEqual(2);
+      expect(Object.keys(service.addPlays({ members: { current: [], past: [] } }).plays).length).toEqual(0);
     });
 
     it('should not add members with empty plays array to any play', () => {
-      
+      const testData: IncompleteBand = {
+        members: {
+          current: [
+            { name: 'a', age: 1, plays: ['play1'] },
+            { name: 'b', age: 2, plays: ['play2', 'play1'] },
+          ],
+          past: [
+              { name: 'c', age: 3, plays: ['play2'] },
+              { name: 'd', age: 3, plays: [] }
+          ]
+        }
+      };
+
+      expect(Object.values(service.addPlays(testData).plays).flat()).toEqual(jasmine.arrayWithExactContents(['a', 'b', 'b', 'c']));
+    });
+
+    it('should not add members twice with duplicate plays', () => {
+      const testDataWithDuplicates = {
+        members: {
+          current: [
+            { name: 'a', age: 1, plays: ['play1'] },
+            { name: 'b', age: 2, plays: ['play2', 'play1'] },
+          ],
+          past: [
+              { name: 'c', age: 3, plays: ['play2', 'play2'] }
+          ]
+        }
+      };
+
+      expect(Object.values(service.addPlays(testDataWithDuplicates).plays).flat()).toEqual(jasmine.arrayWithExactContents(['a', 'b', 'b', 'c']));
     });
 
     it('should add every play once', () => {
-      
+      const testData: IncompleteBand = {
+        members: {
+          current: [
+            { name: 'a', age: 1, plays: ['play1'] },
+            { name: 'b', age: 2, plays: ['play2', 'play1'] },
+          ],
+          past: [
+              { name: 'c', age: 3, plays: ['play2'] },
+              { name: 'd', age: 3, plays: [] }
+          ]
+        }
+      };
+
+      expect(Object.keys(service.addPlays(testData).plays).length).toEqual(jasmine.arrayWithExactContents(['play1', 'play2']));
     });
 
     it('should transform names to lowercase', () => {
-      
+      const testData = {
+        members: {
+          current: [
+            { name: 'Aapo', age: 1, plays: ['play1'] },
+          ],
+          past: [
+              { name: 'wEiRDname', age: 3, plays: ['play2', 'play2'] }
+          ]
+        }
+      };
+
+      expect(Object.values(service.addPlays(testData).plays).flat()).toEqual(jasmine.arrayWithExactContents(['aapo', 'weirdname']));
     });
   });
 });
