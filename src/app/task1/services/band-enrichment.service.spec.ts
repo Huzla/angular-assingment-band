d0e928a0a542b8f1e320ce8e4b9c543b6b738feb
Lari Alakukku <lari@alakukku.net>
2023-11-11 21:24:17 +0200

    Added tests for AddAll method and set up karma

--- src/app/task1/services/band-enrichment.service.spec.ts
+++ src/app/task1/services/band-enrichment.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BandEnrichmentService } from './band-enrichment.service';
+import { IncompleteBand } from '../interfaces/band.interface';
 
 describe('BandEnrichmentService', () => {
   let service: BandEnrichmentService;
@@ -16,15 +17,60 @@ describe('BandEnrichmentService', () => {
 
   describe('AddAll', () => {
     it('should work with empty arrays', () => {
-
+      const testData: IncompleteBand = {
+        members: {
+          current: [
+            {name: 'a', age: 1, plays: []},
+            {name: 'b', age: 2, plays: []},
+          ],
+          past: [
+              {name: 'c', age: 3, plays: []},
+          ]
+        }
+      };
+      
+      expect(service.AddAll(testData).members.all.length).toEqual(3);
+      expect(service.AddAll({ members: { ...testData.members, current: [] } }).members.all.length).toEqual(1);
+      expect(service.AddAll({ members: { ...testData.members, past: [] } }).members.all.length).toEqual(2);
+      expect(service.AddAll({ members: { current: [], past: [] } }).members.all.length).toEqual(0);
     });
 
-    it('should return names from both current and past in lowercase', () => {
+    it('should return names in lowercase', () => {
+      const testData: IncompleteBand = {
+        members: {
+          current: [
+            {name: 'Aapo-Kalevi Jukanpoika', age: 1, plays: []},
+            {name: 'someGuy', age: 2, plays: []},
+          ],
+          past: [
+              {name: 'WeIrD nAME', age: 3, plays: []},
+          ]
+        }
+      };
 
+      const actual = service.AddAll(testData);
+
+      expect(actual.members.all).toEqual(jasmine.arrayWithExactContents(['aapo-kalevi jukanpoika', 'someguy', 'weird name']));
     });
 
-    it('should return names sorted first by age DES and then name ASC', () => {
+    it('should return names sorted first by age DESC and then name ASC', () => {
+      const testData: IncompleteBand = {
+        members: {
+          current: [
+            {name: 'Zorro', age: 1, plays: []},
+            {name: 'someGuy', age: 98, plays: []},
+            {name: 'Aapo-Kalevi Jukanpoika', age: 12, plays: []},
+          ],
+          past: [
+              {name: '', age: 12, plays: []},
+              {name: 'Thor', age: 12, plays: []},
+          ]
+        }
+      };
 
+      const actual = service.AddAll(testData).members.all.map(name => name.toLowerCase());
+
+      expect(actual).toEqual(['zorro', '', 'aapo-kalevi jukanpoika', 'thor', 'someguy']);
     });
   });
 
